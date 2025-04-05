c64a48d06d530c3fb75cf81c00ad08894b23a7a3
aabidahmad <129493662+aabidahmad@users.noreply.github.com>
2025-04-05 17:10:18 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String getMessage(){
-        return "Hello World";
+        return "Hello World new update ";
     }
 }
